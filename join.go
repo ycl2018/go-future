a7ad80afedd123cb3698ff1420a8e3c3c615252e
闫成龙 <yanchenglong.yan@bytedance.com>
2024-11-08 14:45:10 +0800

    feat: add test

--- join.go
+++ join.go
@@ -9,6 +9,9 @@ func unwrap(x []any) any {
 	return x
 }
 
+// Join wait other Future task complete and return a new combined Future2.
+// if the joined Future return one value, combined Future's type 'any' will be the value exactly,
+// or else it's real type is []any.
 func (f *Future[T]) Join(other futureI) *Future2[T, any] {
 	return Go2(func() (T, any, error) {
 		val1, err1 := f.Wait()
@@ -17,6 +20,9 @@ func (f *Future[T]) Join(other futureI) *Future2[T, any] {
 	})
 }
 
+// Join wait other Future task complete and return a new combined Future3.
+// if the joined Future return one value, combined Future's type 'any' will be the value exactly,
+// or else it's real type is []any.
 func (f *Future2[T, V]) Join(other futureI) *Future3[T, V, any] {
 	return Go3(func() (T, V, any, error) {
 		val1, val2, err1 := f.Wait()
@@ -25,6 +31,9 @@ func (f *Future2[T, V]) Join(other futureI) *Future3[T, V, any] {
 	})
 }
 
+// Join wait other Future task complete and return a new combined Future4.
+// if the joined Future return one value, combined Future's type 'any' will be the value exactly,
+// or else it's real type is []any.
 func (f *Future3[T, V, M]) Join(other futureI) *Future4[T, V, M, any] {
 	return Go4(func() (T, V, M, any, error) {
 		val1, val2, val3, err1 := f.Wait()
@@ -33,6 +42,9 @@ func (f *Future3[T, V, M]) Join(other futureI) *Future4[T, V, M, any] {
 	})
 }
 
+// Join wait other Future task complete and return a new combined Future5.
+// if the joined Future return one value, combined Future's type 'any' will be the value exactly,
+// or else it's real type is []any.
 func (f *Future4[T, V, M, N]) Join(other futureI) *Future5[T, V, M, N, any] {
 	return Go5(func() (T, V, M, N, any, error) {
 		val1, val2, val3, val4, err1 := f.Wait()
@@ -41,6 +53,9 @@ func (f *Future4[T, V, M, N]) Join(other futureI) *Future5[T, V, M, N, any] {
 	})
 }
 
+// Join wait other Future task complete and return a new combined Future.
+// in the combined Future's '[6]any',5 values before are current Future's returned values,
+// the last is the combined Future's returned values.
 func (f *Future5[T, V, M, N, O]) Join(other futureI) *Future[[6]any] {
 	return Go(func() ([6]any, error) {
 		val1, val2, val3, val4, val5, err1 := f.Wait()
