4183a7c708e7763cf3135aa5e4f3137e29ed3784
ycl2018 <39751698+ycl2018@users.noreply.github.com>
2024-11-21 16:27:31 +0800

    feat: add Group AnyGroup ErrGroup (#23)
    * feat: add Group AnyGroup ErrGroup

---
+++ future/group.go
@@ -0,0 +1,76 @@
+package future
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+// AnyGroup represents a group of Futures which returns any with an error
+type AnyGroup = Group[any]
+
+// Group represents a group of Futures which returns same type T with an error
+type Group[T any] struct {
+	ff     []*Future[T]
+	sealed atomic.Bool
+}
+
+// Run a worker
+func (g *Group[T]) Run(w worker[T]) {
+	if g.sealed.Load() {
+		panic("group is sealed")
+	}
+	g.ff = append(g.ff, Go(w))
+}
+
+// Add a Future to group
+func (g *Group[T]) Add(f *Future[T]) {
+	if g.sealed.Load() {
+		panic("group is sealed")
+	}
+	g.ff = append(g.ff, f)
+}
+
+// Wait all the Futures return
+func (g *Group[T]) Wait() ([]T, error) {
+	g.sealed.Store(true)
+	return CollectSlice(g.ff...)
+}
+
+// WaitTimeout wait for timeout duration to get all the worker return
+func (g *Group[T]) WaitTimeout(timeout time.Duration) ([]T, error) {
+	g.sealed.Store(true)
+	return CollectSliceTimeout(timeout, g.ff...)
+}
+
+// ErrGroup represents a group of Futures which returns only an error
+type ErrGroup struct {
+	g Group[struct{}]
+}
+
+// Run a worker
+func (e *ErrGroup) Run(w func() error) {
+	e.g.Run(func() (struct{}, error) {
+		err := w()
+		return struct{}{}, err
+	})
+}
+
+// Add a Future to group
+func (e *ErrGroup) Add(f futureI) {
+	e.g.Run(func() (struct{}, error) {
+		_, err := f.waitUnify()
+		return struct{}{}, err
+	})
+}
+
+// Wait all the Futures return
+func (e *ErrGroup) Wait() error {
+	_, err := e.g.Wait()
+	return err
+}
+
+// WaitTimeout wait for timeout duration to get all the worker return
+func (e *ErrGroup) WaitTimeout(timeout time.Duration) error {
+	_, err := e.g.WaitTimeout(timeout)
+	return err
+}
