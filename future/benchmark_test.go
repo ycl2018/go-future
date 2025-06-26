ea011c191af1bfbbb2a1e53141fb75e70b018219
ycl2018 <chenglongustb@gmail.com>
2025-06-26 20:25:14 +0800

    more benchmark

--- future/benchmark_test.go
+++ future/benchmark_test.go
@@ -39,3 +39,81 @@ func BenchmarkWaitGroup1Worker(b *testing.B) {
 	}
 	b.StopTimer()
 }
+
+func BenchmarkFuture3Worker(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var f []*Future[string]
+		for j := 0; j < 3; j++ {
+			f = append(f, Go(func() (string, error) {
+				return "bar", nil
+			}))
+		}
+		CollectSlice(f...)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkWaitGroup3Worker(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var swg sync.WaitGroup
+		swg.Add(3)
+		var strs [3]string
+		var errs [3]error
+		for j := 0; j < 3; j++ {
+			j := j
+			go func() {
+				defer func() {
+					if e := recover(); e != nil {
+						b.Log(e)
+					}
+				}()
+				defer swg.Done()
+				strs[j] = "bar"
+				errs[j] = nil
+			}()
+		}
+		swg.Wait()
+	}
+	b.StopTimer()
+}
+
+func BenchmarkFuture10Worker(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var f []*Future[string]
+		for j := 0; j < 10; j++ {
+			f = append(f, Go(func() (string, error) {
+				return "bar", nil
+			}))
+		}
+		CollectSlice(f...)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkWaitGroup10Worker(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var swg sync.WaitGroup
+		swg.Add(10)
+		var strs [10]string
+		var errs [10]error
+		for j := 0; j < 10; j++ {
+			j := j
+			go func() {
+				defer func() {
+					if e := recover(); e != nil {
+						b.Log(e)
+					}
+				}()
+				defer swg.Done()
+				strs[j] = "bar"
+				errs[j] = nil
+			}()
+		}
+		swg.Wait()
+	}
+	b.StopTimer()
+}
